9abf48edcf585dc1ab703ece293442cc1b3fef33
glblduh <gianlloyd.laureano@gmail.com>
2025-12-05 22:44:51 +0800

    added new endpoints to http router

--- backend/main.go
+++ backend/main.go
@@ -15,12 +15,14 @@ func main() {
 
 	httpRouter.HandleFunc("/api/addemployee", apiAddEmployee).Methods("POST")
 	httpRouter.HandleFunc("/api/updateschedule", apiUpdateSchedule).Methods("POST")
+	httpRouter.HandleFunc("/api/addattendance", apiAddAttendance).Methods("POST")
 
 	httpRouter.HandleFunc("/api/removeemployee", apiRemoveEmployee).Methods("DELETE")
 
 	httpRouter.HandleFunc("/api/getallschedule/{idNumber}", apiGetAllYearsSchedule).Methods("GET")
 	httpRouter.HandleFunc("/api/getschedule/{idNumber}/{schoolYear}", apiGetSchedule).Methods("GET")
 	httpRouter.HandleFunc("/api/getemployee/{idNumber}", apiGetEmployee).Methods("GET")
+	httpRouter.HandleFunc("/api/getattendance", apiGetAttendance).Methods("GET")
 
 	httpCors := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
